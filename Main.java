c8ea90104eb04fa0cdb1ed1136b1303160f9ab46
lenny-louis <57408297+lenny-louis@users.noreply.github.com>
2021-03-23 11:54:57 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue2, Formation Jenkins IPSSI 2!");
+        System.out.println("Bienvenue2, Formation Jenkins IPSSI 3!");
     }
 }
